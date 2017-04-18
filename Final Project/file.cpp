337732a2d39df1369558e2389fc68b1ca103a1f9
Josh <bluedude227@gmail.com>
2017-04-17 20:15:24 -0500

    idk

--- Final Project/file.cpp
+++ Final Project/file.cpp
@@ -8,17 +8,30 @@ int main() {
 	
 	ifstream infile("sudoku1.txt");
 	string line;
+	int board[9][9];
+	int r = 0;
 	
 	while (getline(infile, line)) {
 		
 		istringstream iss(line);
-		int row[9];
-		for (int i = 0; i<=8; i++)
-			if (!(iss >> row[i])) { break; } // error
+		for (int j = 0; j<=8; j++)
+			if (!(iss >> board[r][j])) { break; } // error
+		r++;
+	}
 
-    for (int i = 0; i<=8; i++)
-	   cout << row[i] << " ";
-   
-	cout << endl;
+	//Print sudoku board with formating
+	for (int i = 0; i<=8; i++) {
+		for (int j = 0; j<=8; j++)
+		{
+			cout << board[i][j];
+			
+			if (j == 2 || j == 5)
+				cout << " | ";
+			else cout << " ";
+			
+		}
+		cout << endl;
+		if (i == 2 || i == 5)
+			cout << "----------------------" << endl;
 	}
 }
